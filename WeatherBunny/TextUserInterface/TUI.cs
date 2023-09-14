8bc1a4c4f79b4b7367f95b06af122c759a31e2ac
Rasmus Lyngberg <113756357+esserdk88@users.noreply.github.com>
2023-09-14 17:26:14 +0200

    Simple TUI and nav to WB

---
+++ WeatherBunny/TextUserInterface/TUI.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherBunny;
+
+namespace TextUserInterface
+{
+    internal class TUI
+    {
+        public static void Main(string[] args)
+        {
+            //Start of the TUI
+            State _currentState = State.MAINMENU;
+
+            while(_currentState != State.EXIT)
+            {
+                State nextState = State.MAINMENU; //MainMenu
+                switch (_currentState)
+                {
+                    case State.MAINMENU:
+                        nextState = MainMenu();
+                        break;
+
+                    case State.TECHNOLOGY:
+                        nextState = TechnologyMenu();
+                        break;
+
+                    case State.WEATHERBUNNY:
+                        nextState = State.TECHNOLOGY;
+                        //Run Weather Bunny
+                        Console.Clear();
+                        WeatherBunny.Program.Start();
+
+                        break;
+
+                    //More cases can be added here
+
+                    default: 
+                        nextState = State.MAINMENU; 
+                        break;
+
+                        
+                }
+                _currentState = nextState;
+            }
+            Console.Clear();
+            Console.WriteLine("The program will now exit. press enter to close");
+        }
+
+        private static State TechnologyMenu()
+        {
+            Console.Clear();
+            State outputState = State.MAINMENU;
+            Console.WriteLine("---------- Technology ----------");
+            Console.WriteLine("");
+            Console.WriteLine("(W) - WeatherBunny");
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("(enter) - Main Menu");
+            Console.WriteLine("(0) - Exit");
+            Console.WriteLine("");
+            Console.Write("Choose: ");
+            char key = Console.ReadKey().KeyChar;
+
+            switch (key)
+            {
+                case 'w':
+                    outputState = State.WEATHERBUNNY;
+                    break;
+                case '0':
+                    outputState = State.EXIT;
+                    break;
+            }
+
+            return outputState;
+        }
+
+        private static State MainMenu()
+        {
+            Console.Clear();
+            State outputState = State.MAINMENU;
+            Console.WriteLine("---------- Main Menu ----------");
+            Console.WriteLine("");
+            Console.WriteLine("(T) - Technology");
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("(0) - Exit");
+            Console.WriteLine("");
+            Console.Write("Choose: ");
+            char key = Console.ReadKey().KeyChar;
+
+            switch (key)
+            {
+                case 't':
+                    outputState = State.TECHNOLOGY;
+                    break;
+                case '0':
+                    outputState = State.EXIT;
+                    break;
+            }
+
+            return outputState;
+        }
+    }
+}
--- WeatherBunny/WeatherBunny/Program.cs
+++ WeatherBunny/WeatherBunny/Program.cs
@@ -1,22 +1,31 @@
-﻿// Module 02, Network, Exercise
-// Based on
-//   https://scrapingant.com/blog/html-parsing-libraries-c-sharp
-//   https://dotnetfiddle.net/HoXY9S
-//   https://www.w3schools.com/xml/xpath_syntax.asp
-
-
-using HtmlAgilityPack;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Net;
 
-Console.WriteLine("Hi, I will be your weatherbunny today");
-var url = "https://www.timeanddate.no/vaer/?continent=europe&low=c";
-var web = new HtmlWeb();
-var dom = web.Load(url);
-var cities = dom.DocumentNode.SelectNodes("//td");
-Console.WriteLine("Hi, I will be your weatherbunny today");
-for (int index=0; index<cities.Count; index += 4)
+namespace WeatherBunny
 {
-    string temperature = cities[index + 3].InnerText;
-    int posAmbersand = temperature.IndexOf('&');
-    Console.WriteLine(cities[index].InnerText + "   " + cities[index+3].InnerText.Remove(posAmbersand, 6));
+    public class Program
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Hi, I will be your weatherbunny today");
+            var url = "https://www.timeanddate.no/vaer/?continent=europe&low=c";
+            var web = new HtmlWeb();
+            var dom = web.Load(url);
+            var cities = dom.DocumentNode.SelectNodes("//td");
+            Console.WriteLine("Hi, I will be your weatherbunny today");
+            for (int index = 0; index < cities.Count; index += 4)
+            {
+                string temperature = cities[index + 3].InnerText;
+                int posAmbersand = temperature.IndexOf('&');
+                Console.WriteLine(cities[index].InnerText + "   " + cities[index + 3].InnerText.Remove(posAmbersand, 6));
+            }
+            Console.ReadLine();
+
+        }
+    }
 }
